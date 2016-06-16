8dcfa2cb74e9c7b46bc10a90a69585657ee0110a
Kathryn Reagan <kreagan@thoughtworks.com>
2016-06-16 13:45:14 -0500

    completed first user story

--- src/main/java/com/thoughtworks/tictactoe/Main.java
+++ src/main/java/com/thoughtworks/tictactoe/Main.java
@@ -5,5 +5,6 @@ import java.io.PrintStream;
 public class Main {
     public static void main(String[] args) {
         Game game = new Game(new PrintStream(System.out));
+        game.start();
     }
 }
